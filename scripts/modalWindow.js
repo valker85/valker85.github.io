b65735b0a47694e6485c712dc3c2444d08fbfe1e
Daniil Kudryashov <den111vv9@mail.ru>
2023-03-01 17:28:48 +0300

    final desktop version

--- scripts/modalWindow.js
+++ scripts/modalWindow.js
@@ -1,7 +1,10 @@
-// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
-
+// Элементы при нажатии которых открывается модальное окно
 const openPopUp = Array.from(document.querySelectorAll('.project'))
+
+// Крестик в модальном окне
 const closePopUp = document.querySelector('.pop_up_close')
+
+// Модальное окно
 const popUp = document.querySelector('.pop_up')
 
 // Для запрета скролла
@@ -13,72 +16,96 @@ const popUpContainer = document.querySelector('.pop_up_container')
 // Текст модального окна
 const popUpText = document.querySelector('.popUpText')
 
+// Родительский элемент слайдов
+const swiperBody = document.querySelector('.swiper-wrapper')
+
+// Контент слайдера
 const popUpState = {
     'modal1': {
-        title: 'Пример моей верстки по макету'
+        title: 'Пример моей верстки по макету',
+        images: ['./images/portfolio/carMusc/fullScreen.png']
     },
     'modal2': {
-        title: 'Пример моей верстки по макету'
+        title: 'Пример моей верстки по макету',
+        images: ['./images/portfolio/ritmStyle/fullScreen.png']
     },
     'modal3':{
-        title: 'Crypto Exchange'
+        title: 'Crypto Exchange',
+        images: ['./images/portfolio/cryptoExchange/fullScreen.png']
     },
     'modal4': {
-        title: 'NFT Airdrop'
+        title: 'NFT Airdrop',
+        images: ['./images/portfolio/nftAirdrop/fullScreen.png']
     },
     'modal5': {
-        title: 'Yukigassen'
+        title: 'Yukigassen',
+        images: ['./images/portfolio/yukigassen/fullScreen.png']
     },
     'modal6':{
-        title: 'Project Hope'
+        title: 'Project Hope',
+        images: ['./images/portfolio/projectHope/fullScreen.png']
     },
     'modal7': {
-        title: 'CoinPay'
+        title: 'CoinPay',
+        images: ['./images/portfolio/coinPay/fullScreen1.png', './images/portfolio/coinPay/fullScreen2.png', './images/portfolio/coinPay/fullScreen3.png']
     },
     'modal8': {
-        title: 'BinaryX'
+        title: 'BinaryX',
+        images: ['./images/portfolio/binaryX/home.png', './images/portfolio/binaryX/game.png', './images/portfolio/binaryX/dao.png', './images/portfolio/binaryX/whitePaper.png']
     },
     'modal9':{
-        title: 'The House'
+        title: 'The House',
+        images: ['./images/portfolio/theHouse/home.png', './images/portfolio/theHouse/brands.png', './images/portfolio/theHouse/description.png', './images/portfolio/theHouse/productCard.png', './images/portfolio/theHouse/recipes.png']
     },
     'modal10': {
-        title: 'Konyak Hotel'
+        title: 'Konyak Hotel',
+        images: ['./images/portfolio/konyakHotel/home.png', './images/portfolio/konyakHotel/rooms.png', './images/portfolio/konyakHotel/roomPage.png', './images/portfolio/konyakHotel/restaurant.png', './images/portfolio/konyakHotel/events.png', './images/portfolio/konyakHotel/booking.png', './images/portfolio/konyakHotel/about.png', './images/portfolio/konyakHotel/contactUs.png', './images/portfolio/konyakHotel/homeMobile.png', './images/portfolio/konyakHotel/mobileMenu.png']
     },
     'modal11': {
-        title: 'ProkatAM'
+        title: 'ProkatAM',
+        images: ['./images/portfolio/prokatAm/home.png','./images/portfolio/prokatAm/menu.png', './images/portfolio/prokatAm/contactUs.png', './images/portfolio/prokatAm/products.png']
     },
     'modal12':{
-        title: 'MrFast'
+        title: 'MrFast',
+        images: ['./images/portfolio/mrFast/home.png', './images/portfolio/mrFast/store.png', './images/portfolio/mrFast/product.png', './images/portfolio/mrFast/pricing.png', './images/portfolio/mrFast/blog.png', './images/portfolio/mrFast/faq.png', './images/portfolio/mrFast/login.png']
     }
 }
 
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
+// Лоадер 
+let loader = document.createElement("div")
+loader.classList.add('lds-ring')
+setTimeout(() => {
+    loader.appendChild(document.createElement("div"))
+    loader.appendChild(document.createElement("div"))
+    loader.appendChild(document.createElement("div"))
+    loader.appendChild(document.createElement("div"))
+}, 400)
 
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//         dynamicBullets: true,
-//     },
-
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-
-//     // And if we need scrollbar
-//     // scrollbar: {
-//     //     el: '.swiper-scrollbar',
-//     // },
-// });
 
 function modalWindow(id) {
     openPopUpFn()
-
     popUpText.innerHTML = popUpState[id].title
+    sliderContent(popUpState[id].images)
+}
+
+// Контент для слайдеров
+function sliderContent(content) {
+    swiperBody.replaceChildren()
+    swiperBody.appendChild(loader)
+
+    setTimeout(() => {
+        swiperBody.replaceChildren()
+        for (let i = 0; i < content.length; i++) {
+            let slide = document.createElement("div")
+            let img = document.createElement("img")
+    
+            slide.classList.add('swiper-slide')
+            img.src = content[i]
+    
+            slide.appendChild(img)
+            swiperBody.appendChild(slide)
+        }
+    }, 500);
 }
 
 // Закрываем модальное окно с помощью кнопки 'Esc'
@@ -100,7 +127,7 @@ popUpContainer.addEventListener('click', (event)=>{
     }
 })
 
-// Ф-ции открывающие/закрывающие модальное окно
+// Функции открывающие/закрывающие модальное окно
 function closePopUpFn() {
     popUp.classList.remove('active')
     body.style.overflow = 'auto'
